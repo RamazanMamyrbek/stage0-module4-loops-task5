dd1c94f5c18d795363f3f1f13e5be9559d325209
RamazanMamyrbek <rama.mamirbek@gmail.com>
2023-09-13 12:44:41 +0600

    l

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int crossIndex = (int)Math.floor((sideLength+1)/2);
+        for(int i = 1; i <= sideLength; i++) {
+            if(i==crossIndex) {
+                for(int j = 1; j <= sideLength; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }else {
+                for(int j = 1; j < crossIndex; j++)
+                    System.out.print(" ");
+                System.out.print("8");
+                for(int j = 1; j < crossIndex; j++)
+                    System.out.print(" ");
+                System.out.println();
+            }
 
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (height - i); j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
 
+        // Print the bottom part of the hourglass
+        for (int i = height - 2; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (height - i); j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
